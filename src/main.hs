b1d21bf816262079fb532e02b41d4818e970368e
Nicola Sartorato <niksart@airmail.cc>
2018-09-30 19:07:05 +0200

    Add plural and conjugation conversion

--- src/main.hs
+++ src/main.hs
@@ -1,36 +1,68 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Set (Set, fromList, member)
+import Data.Set as Set
 import Debug.Trace
 import Data.Char
 import Data.Maybe
-import Data.Aeson
-import Data.ByteString.Lazy hiding (map, interact, filter, length)
+import Data.List.Split as Split
+import Data.Map.Strict as Map
 
 hasSpaces [] = False
 hasSpaces (l:ll) | l == ' '  = True
                  | otherwise = hasSpaces ll
 
-getPercentage:: Set String -> [String] -> Int
-getPercentage dictWords inputWords = 100 - ((getPercentage' inputWords dictWords 0 * 100) `div` (length inputWords))
 
-getPercentage' [] dictWords counter = counter
-getPercentage' (w:ww) dictWords counter 
-  | member w dictWords = getPercentage' ww dictWords (counter + 1)
-  | otherwise          = trace w $ getPercentage' ww dictWords counter
+getComplexityPercentage:: Set String -> [String] -> Int
+getComplexityPercentage dictWords inputWords = 100 - ((getComplexityPercentage' inputWords dictWords 0 * 100) `div` (length inputWords))
+
+getComplexityPercentage' [] dictWords counter = counter
+getComplexityPercentage' (w:ww) dictWords counter 
+  | Set.member w dictWords = getComplexityPercentage' ww dictWords (counter + 1)
+  | otherwise          = trace w $ getComplexityPercentage' ww dictWords counter
+
+-- indexLemma Frequency derivedLemmas
+data Lemma = Lemma String Int (Set String) deriving (Show)
+
+-- getting a lemma from a line
+mySplit = Split.splitOn " -> "
+mySplitHead = Split.splitOn "/"
+mySplitTail = Split.splitOn ","
+
+getLemmaFromLine :: String -> Lemma
+getLemmaFromLine line = Lemma 
+  (head (mySplitHead (head (mySplit line)))) 
+  (read (last (mySplitHead (head (mySplit line)))) :: Int) 
+  (Set.fromList (mySplitTail (last (mySplit line))))
+
+
+getMapFromLemma :: Lemma -> (Map String String)
+getMapFromLemma (Lemma indexLemma _ derivedLemmas) = Map.fromSet (\k -> indexLemma) derivedLemmas
+
+getMapFromLemmasList' [] dict = dict
+getMapFromLemmasList' (l:ll) dict = getMapFromLemmasList' ll (Map.union dict (getMapFromLemma l))
+
+getMapFromLemmasList lemmas = getMapFromLemmasList' lemmas Map.empty
+
+
+convertLemma lemmasMap word = 
+  if Map.lookup word lemmasMap == Nothing then word else (fromJust(Map.lookup word lemmasMap))
+
 
 main = do
-  words3000String <- Prelude.readFile "resources/oxford-3000.txt"
-  let words3000Set = fromList $ filter (not . hasSpaces) (lines words3000String)
+  words3000String <- readFile "resources/oxford-3000.txt"
+  let words3000Set = Set.fromList $ Prelude.filter (not . hasSpaces) (lines words3000String)
   
-  verbs3000JSON <- Data.ByteString.Lazy.readFile "resources/verbs/verbs-oxford-3000.json"
-  let verbs3000Set = fromList $ fromJust (decode verbs3000JSON :: Maybe [String])
-       
+  lemmasString <- readFile "resources/lemmas_en.txt"
+  let lemmas = Prelude.map getLemmaFromLine (lines lemmasString)
+  
+  let lemmasMap = getMapFromLemmasList lemmas
+  
   interact $
     (++ " % of the words are not inside the Oxford 3000 list.\n") .
     show .
-    getPercentage words3000Set .
-    map (filter isAlpha) . -- delete punctuation
-    map (map toLower) . -- lowercase characters
+    getComplexityPercentage words3000Set .
+    Prelude.map (convertLemma lemmasMap) . -- convert coniugated words
+    Prelude.map (Prelude.filter isAlpha) . -- delete punctuation
+    Prelude.map (Prelude.map toLower) . -- lowercase characters
     words
        
