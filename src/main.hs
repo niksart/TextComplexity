345977d868c4f1d6f1931badc26eb49000c9e359
Nicola Sartorato <niksart@airmail.cc>
2018-10-02 14:37:04 +0200

    Spit module into Parser and TextComplexity, fix 'I'  issue, '\n' issue and abbreviations issue

--- src/main.hs
+++ src/main.hs
@@ -1,68 +1,64 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Set as Set
+import qualified Data.Set as Set
 import Debug.Trace
 import Data.Char
-import Data.Maybe
-import Data.List.Split as Split
-import Data.Map.Strict as Map
+import qualified Data.Maybe as Maybe
+import qualified Data.Map.Strict as Map
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Aeson as Aeson
 
+-- | is c a meaningful character?
+isAdmissible :: Char -> Bool
+isAdmissible c = isAlpha c || c == '\''
+
+convertCRLFtoChar :: Char -> Char
+convertCRLFtoChar '\n' = ' '
+convertCRLFtoChar c = c
+
+-- | convert to lowercase all the characters apart 'I'
+convertToLower :: String -> String
+convertToLower [] = []
+convertToLower (c:cc) = if (c:cc) == "I" then "I" else (toLower c):(convertToLower cc)
+
+spaceBeforeQuote' :: Char -> String
+spaceBeforeQuote' '\'' = " '"
+spaceBeforeQuote' c = [c]
+
+spaceBeforeQuote :: String -> String
+spaceBeforeQuote [] = []
+spaceBeforeQuote (s:ss) = (spaceBeforeQuote' s) ++ (spaceBeforeQuote ss)
+
+hasSpaces :: String -> Bool
 hasSpaces [] = False
 hasSpaces (l:ll) | l == ' '  = True
                  | otherwise = hasSpaces ll
 
-
-getComplexityPercentage:: Set String -> [String] -> Int
+getComplexityPercentage :: Set.Set String -> [String] -> Int
 getComplexityPercentage dictWords inputWords = 100 - ((getComplexityPercentage' inputWords dictWords 0 * 100) `div` (length inputWords))
 
 getComplexityPercentage' [] dictWords counter = counter
 getComplexityPercentage' (w:ww) dictWords counter 
   | Set.member w dictWords = getComplexityPercentage' ww dictWords (counter + 1)
-  | otherwise          = trace w $ getComplexityPercentage' ww dictWords counter
-
--- indexLemma Frequency derivedLemmas
-data Lemma = Lemma String Int (Set String) deriving (Show)
-
--- getting a lemma from a line
-mySplit = Split.splitOn " -> "
-mySplitHead = Split.splitOn "/"
-mySplitTail = Split.splitOn ","
-
-getLemmaFromLine :: String -> Lemma
-getLemmaFromLine line = Lemma 
-  (head (mySplitHead (head (mySplit line)))) 
-  (read (last (mySplitHead (head (mySplit line)))) :: Int) 
-  (Set.fromList (mySplitTail (last (mySplit line))))
-
-
-getMapFromLemma :: Lemma -> (Map String String)
-getMapFromLemma (Lemma indexLemma _ derivedLemmas) = Map.fromSet (\k -> indexLemma) derivedLemmas
-
-getMapFromLemmasList' [] dict = dict
-getMapFromLemmasList' (l:ll) dict = getMapFromLemmasList' ll (Map.union dict (getMapFromLemma l))
-
-getMapFromLemmasList lemmas = getMapFromLemmasList' lemmas Map.empty
-
+  | otherwise              = trace w $ getComplexityPercentage' ww dictWords counter
 
 convertLemma lemmasMap word = 
-  if Map.lookup word lemmasMap == Nothing then word else (fromJust(Map.lookup word lemmasMap))
+  if Map.lookup word lemmasMap == Nothing then word else (Maybe.fromJust(Map.lookup word lemmasMap))
 
 
 main = do
   words3000String <- readFile "resources/oxford-3000.txt"
   let words3000Set = Set.fromList $ Prelude.filter (not . hasSpaces) (lines words3000String)
   
-  lemmasString <- readFile "resources/lemmas_en.txt"
-  let lemmas = Prelude.map getLemmaFromLine (lines lemmasString)
-  
-  let lemmasMap = getMapFromLemmasList lemmas
+  lemmasMapString <- BS.readFile "resources/lemmasMap.json"
+  let lemmasMap = Maybe.fromJust (Aeson.decode lemmasMapString :: Maybe (Map.Map String String))
   
   interact $
     (++ " % of the words are not inside the Oxford 3000 list.\n") .
     show .
     getComplexityPercentage words3000Set .
-    Prelude.map (convertLemma lemmasMap) . -- convert coniugated words
-    Prelude.map (Prelude.filter isAlpha) . -- delete punctuation
-    Prelude.map (Prelude.map toLower) . -- lowercase characters
-    words
+    filter (/= "") .
+    map (convertLemma lemmasMap) . -- convert coniugated words
+    map (filter isAdmissible) . -- delete punctuation
+    map convertToLower . -- lowercase characters
+    words .
+    spaceBeforeQuote
        
---
+++ src/parser.hs
@@ -0,0 +1,36 @@
+import qualified Data.List.Split as Split
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as B
+
+-- indexLemma derivedLemmas
+data Lemma = Lemma String (Set.Set String) deriving (Show)
+
+-- getting a lemma from a line
+mySplit = Split.splitOn " -> "
+mySplitHead = Split.splitOn "/"
+mySplitTail = Split.splitOn ","
+
+getLemmaFromLine :: String -> Lemma
+getLemmaFromLine line = Lemma 
+  (head (mySplitHead (head (mySplit line)))) 
+  (Set.fromList (mySplitTail (last (mySplit line))))
+
+getMapFromLemma :: Lemma -> (Map.Map String String)
+getMapFromLemma (Lemma indexLemma derivedLemmas) = Map.fromSet (\k -> indexLemma) derivedLemmas
+
+getMapFromLemmasList' [] dict = dict
+getMapFromLemmasList' (l:ll) dict = getMapFromLemmasList' ll (Map.union dict (getMapFromLemma l))
+
+getMapFromLemmasList lemmas = getMapFromLemmasList' lemmas Map.empty
+
+
+main = do
+  lemmasString <- readFile "resources/lemmas_en.txt"
+  let lemmas = Prelude.map getLemmaFromLine (lines lemmasString)
+  
+  let lemmasMap = getMapFromLemmasList lemmas
+  
+  B.writeFile "resources/lemmasMap.json" (Aeson.encode lemmasMap)
+  
